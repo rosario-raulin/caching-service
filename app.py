671eb9fb63f708331945d66276f31627d91bd92d
Rosario Raulin <raulin@st.ovgu.de>
2012-12-19 21:21:24 +0100

    add diligant variant

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import hmac
 import hashlib
 import zipfile
 import pylibmc
+import threading
+import time
 
 app = flask.Flask(__name__)
 app.debug = True
@@ -44,9 +46,24 @@ def is_last_byte_even(inp):
 	return array[len(array) - 1] % 2 == 0
 
 def create_and_calc(x):
-	fname = '/tmp/foo%d.zip' % x
+	fname = '/tmp/foo%d-%f.zip' % (x, time.time())
 	gen_random_file(fname, x * 1024 * 1024)
 	return str(is_last_byte_even(calc_hmac(fname)))
+
+class DiligentWorker(threading.Thread):
+	lock = threading.Lock()
+
+	def __init__(self, x):
+		threading.Thread.__init__(self)
+		self.x = x
+		self.xs = str(x)
+		
+	def run(self):
+		if not self.xs in CACHE:
+			value = create_and_calc(self.x)
+			DiligentWorker.lock.acquire()
+			CACHE[self.xs] = value
+			DiligentWorker.lock.release()
 
 @app.route('/<int:x>')
 def service(x):
@@ -66,6 +83,36 @@ def cache_service(x, negative = False):
 @app.route('/negative/<int:x>')
 def negative_service(x):
 	return cache_service(x, True)
+
+THREADS = []
+
+@app.route('/diligent/<int:x>')
+def diligent_service(x):
+
+	global THREADS
+	ts = []
+	for thread in THREADS:
+		thread.join(0.001)
+		if thread.isAlive():
+			ts.append(thread)
+	print ts
+	THREADS = ts
+	print THREADS
+
+	xs = str(x)
+	if xs in CACHE:
+		val = CACHE[xs]
+		w1 = DiligentWorker(x + 1)
+		w2 = DiligentWorker(x - 1)
+		THREADS.append(w1)
+		THREADS.append(w2)
+		w1.start()
+		w2.start()
+		return val
+	else:
+		value = create_and_calc(x)
+		CACHE[xs] = value
+		return value
 
 @app.route('/clear')
 def clear_cache():
