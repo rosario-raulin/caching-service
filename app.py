06a90fbaa5352fbb434ea94f6be4c42cb021d222
Rosario Raulin <raulin@st.ovgu.de>
2012-12-19 19:04:39 +0100

    fix memcache access

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ CACHE = pylibmc.Client(
     servers=[os.environ.get('MEMCACHE_SERVERS')],
 	username=os.environ.get('MEMCACHE_USERNAME'),
 	password=os.environ.get('MEMCACHE_PASSWORD'),
-	binary=True
 )
 
 def gen_random_file(path, size):
